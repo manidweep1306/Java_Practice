24bb43a241f2f03dc99cab65b934c5d628a46730
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2026-05-07 23:58:02 +0530

    Add comments for calculations in Expression.java

---
+++ Expression.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        float a = 7/4*9/2;
+        System.out.print(a);
+    }
+}
+
+//4
+//7/4 = 1
+//1*9 = 9
+//9/2 = 4
