5e5fb6200df26f072122442f39a3751cc2611038
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2026-05-07 22:45:46 +0530

    Create SumOfThreeNumbers.java

---
+++ SumOfThreeNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        System.out.println("Sum of 3 numbers: " + (a + b + c));
+    }
+}
