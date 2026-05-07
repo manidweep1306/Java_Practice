ba98331f64c6fed85051ff9d726c0ce4c7490e3c
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2026-05-07 23:38:10 +0530

    Add Name&amp;Greet.java for greeting user by name

---
+++ Name&Greet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter name:");
+        String a = s.next();
+        System.out.printf("Hello %s, have a good day ",a);
+    }
+}
